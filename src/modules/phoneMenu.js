ae560b17da609b90b4d1a633c8363d1dabc8aa9b
Adam Forgac <aforg@email.cz>
2023-06-17 02:09:38 +0200

    Remove animation bug

--- src/modules/phoneMenu.js
+++ src/modules/phoneMenu.js
@@ -3,6 +3,38 @@ function createPhoneMenu() {
   const mobileMenu = document.querySelector('.nav');
   const taskBoard = document.querySelector('.task-board');
   const body = document.querySelector('body');
+  const options = document.querySelectorAll('.options div');
+  const listOptions = document.querySelectorAll('.project-list');
+
+  options.forEach((option) => {
+    option.addEventListener('click', () => {
+      mobileMenu.style.pointerEvents = 'none';
+      mobileMenu.style.left = '-300px';
+      mobileMenu.style.width = '10%';
+      mobileMenu.style.opacity = '1';
+
+      taskBoard.style.pointerEvents = 'all';
+      taskBoard.style.filter = 'none';
+
+      body.style.overflow = 'visible';
+      mobileMenu.classList.remove('active-nav');
+    });
+  });
+
+  listOptions.forEach((option) => {
+    option.addEventListener('click', () => {
+      mobileMenu.style.pointerEvents = 'none';
+      mobileMenu.style.left = '-300px';
+      mobileMenu.style.width = '10%';
+      mobileMenu.style.opacity = '1';
+
+      taskBoard.style.pointerEvents = 'all';
+      taskBoard.style.filter = 'none';
+
+      body.style.overflow = 'visible';
+      mobileMenu.classList.remove('active-nav');
+    });
+  });
 
   mobileMenuIcon.addEventListener('click', () => {
     mobileMenu.style.pointerEvents = 'all';
